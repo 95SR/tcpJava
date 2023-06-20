9f780cd0fb5faedc3a7c7f255cd5cc8b62be1b03
syahri <greensketch26@gmail.com>
2023-06-20 18:29:33 +0900

    edit Client

--- client/Client.java
+++ client/Client.java
@@ -1,9 +1,27 @@
 package client;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
+    //fields
+    public static InetAddress ia;
 
     public static void clientStart(int port){
+        try  {
 
+            ia = InetAddress.getLocalHost();
+            Socket socket = new Socket(ia, port);
+            
+        }catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
     
 }
